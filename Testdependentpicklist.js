53f4df93392da406b1d47cc6bf435104f26bf2f7
Akash <annindyadas@gmail.com>
2025-08-25 16:49:34 -0400

    Update Testdependentpicklist.js

--- Testdependentpicklist.js
+++ Testdependentpicklist.js
@@ -3,26 +3,26 @@ import { LightningElement,wire, api, track  } from 'lwc';
 import {getObjectInfo} from 'lightning/uiObjectInfoApi';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import {getPicklistValues} from 'lightning/uiObjectInfoApi';
-import SLA_FIELD from '@salesforce/schema/Account.SLA__c';
-import UPSELL_FIELD from '@salesforce/schema/Account.UpsellOpportunity__c';
+import Category_FIELD from '@salesforce/schema/Account.Category__c';
+import SubCategory_FIELD from '@salesforce/schema/Account.SubCategory__c';
 
 export default class Testdependentpicklist extends LightningElement {
     @wire(getObjectInfo, {objectApiName: ACCOUNT_OBJECT })
     accountInfo;
 
-    @track slaOptions;
-    @track upsellOptions;
+    @track categoryOptions;
+    @track subCategoryOptions;
 
-    @wire(getPicklistValues, {recordTypeId: '$accountInfo.data.defaultRecordTypeId', fieldApiName: SLA_FIELD })
-    slaFieldInfo({ data, error }) {
-        if (data) this.slaFieldData = data;
+    @wire(getPicklistValues, {recordTypeId: '$accountInfo.data.defaultRecordTypeId', fieldApiName: Category_FIELD })
+    categoryFieldInfo({ data, error }) {
+        if (data) this.categoryOptions = data;
     }
-    @wire(getPicklistValues, {recordTypeId:'$accountInfo.data.defaultRecordTypeId', fieldApiName: UPSELL_FIELD })
-    upsellFieldInfo({ data, error }) {
-        if (data) this.upsellOptions = data.values;
+    @wire(getPicklistValues, {recordTypeId:'$accountInfo.data.defaultRecordTypeId', fieldApiName: SubCategory_FIELD })
+    subCategoryFieldInfo({ data, error }) {
+        if (data) this.subCategoryOptions = data.values;
     }
-    handleUpsellChange(event) {
-        let key = this.slaFieldData.controllerValues[event.target.value];
-        this.slaOptions = this.slaFieldData.values.filter(opt => opt.validFor.includes(key));
+    handleCategoryChange(event) {
+        let key = this.categoryFieldData.controllerValues[event.target.value];
+        this.subCategoryOptions = this.subCategoryFieldData.values.filter(opt => opt.validFor.includes(key));
     }
 }
